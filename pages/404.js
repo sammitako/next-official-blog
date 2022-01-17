0c8c2b933aa62df606c432db2e25c9b2c659f90c
Hansaem Park <hansaempark@sunssc.com>
2022-01-17 16:43:33 +0900

    Official Tutorial

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Layout from '../components/layout'
+import styles from '../components/layout.module.css'
+
+export default function Custom404() {
+    return (
+        <Layout>
+            <h1 className={styles.header}>PAGE IS NOT FOUND ! ! !</h1>
+        </Layout>
+
+        
+    )
+}
